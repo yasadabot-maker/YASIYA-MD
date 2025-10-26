edb619d8ae46cf39a14e285a9a2113149e74c050
yasadabot-maker <yasadabot@gmail.com>
2025-10-26 18:46:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~ZBxVVBKJ#vFwDkzC6RScytBwv0NFhCGcMu49Uf1UKSuN86pFe2s4'
 };
